0e78f64cee4fb59a90a5ebb9d70798594d9056b0
0wner <0wner@domain.com>
2014-03-22 16:24:48 -0400

    my_cookbook initial commit

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'my_cookbook'
+maintainer       'dpe company'
+maintainer_email 'support@dpeorg.com'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
